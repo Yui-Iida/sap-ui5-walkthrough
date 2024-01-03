3ba5504060255fe52d18e7d15efc04075b8ae274
Yui Iida <yui@Yui.fritz.box>
2024-01-03 16:19:54 +0100

    add message toast

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,13 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-  return Controller.extend("sap.ui.demo.walkthrough.App", {
-    onShowHello: function () {
-      // Show a native or vanailla JS alert
-      alert("Hello there!");
-    },
-  });
-});
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.App", {
+      onShowHello: function () {
+        // Show a native or vanailla JS alert
+        // alert("Hello there!");
+        MessageToast.show("Hello there!");
+      },
+    });
+  }
+);
